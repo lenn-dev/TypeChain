06519715cf8b753ff0427b61ef91bbc8299a1168
soo <adellakwon@gmail.com>
2024-02-23 20:07:12 +1300

    Feat: type declaration

--- src/myPackage.js
+++ src/myPackage.js
@@ -2,22 +2,22 @@
 // 깃헙과 npm에 누군가 이 코드파일을 push해 두고 우리가 설치했다고 가정해보자
 // index.ts 파일에서 myPackage.js파일이 node의 모듈인것처럼 import 해보자
 
-// @ts-check
-/**
- * Initializes
- * @param {object} config
- * @param {boolean} config
- * @param {string} config 
- * @returns {boolean}
- */
+// // @ts-check
+// /**
+//  * Initializes
+//  * @param {object} config
+//  * @param {boolean} config
+//  * @param {string} config 
+//  * @returns {boolean}
+//  */
 export function init(config){
     return true;
 }
-/**
- * 
- * @param {number} code 
- * @returns {number}
- */
+// /**
+//  * 
+//  * @param {number} code 
+//  * @returns {number}
+//  */
 export function exit(code){
     return code + 1;
 }
