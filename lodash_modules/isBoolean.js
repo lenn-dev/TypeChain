0aeb27e1586438910c7614cc5f633416465fa15d
soo <adellakwon@gmail.com>
2024-02-23 20:06:30 +1300

    Feat: lodash module

---
+++ lodash_modules/isBoolean.js
@@ -0,0 +1,29 @@
+//https://github.com/lodash/lodash/blob/main/src/isBoolean.ts
+
+import getTag from './.internal/getTag.js';
+import isObjectLike from './isObjectLike.js';
+
+/**
+ * Checks if `value` is classified as a boolean primitive or object.
+ *
+ * @since 0.1.0
+ * @category Lang
+ * @param {*} value The value to check.
+ * @returns {boolean} Returns `true` if `value` is a boolean, else `false`.
+ * @example
+ *
+ * isBoolean(false)
+ * // => true
+ *
+ * isBoolean(null)
+ * // => false
+ */
+function isBoolean(value) {
+    return (
+        value === true ||
+        value === false ||
+        (isObjectLike(value) && getTag(value) === '[object Boolean]')
+    );
+}
+
+export default isBoolean;
